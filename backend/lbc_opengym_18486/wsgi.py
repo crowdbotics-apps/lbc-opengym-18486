4e847daa92883062e4f7945406623dedfe4ebcf8
Crowdbotics <team@crowdbotics.com>
2020-06-29 17:04:48 +0000

    Initial commit.

---
+++ backend/lbc_opengym_18486/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lbc_opengym_18486 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lbc_opengym_18486.settings')
+
+application = get_wsgi_application()
